7c6a35bbe21c9db0a8c5fcad56543f4939e14974
Andriy Hetsko <andriy.hetsko@boostylabs.com>
2025-12-12 10:10:11 +0100

    add vpc

--- micro-project/vpc/variables.tf
+++ micro-project/vpc/variables.tf
@@ -1,22 +1,27 @@
-variable "vpc_settings" {
-  description = "Settings for ec2 instance"
-  type = object({
-    prefix      = string
-    cidr            = string
-    azs             = list(string)
-    private_subnets = list(string)
-    public_subnets  = list(string)
-  })
-}
-
 variable "project_name" {
-    type = string
-    default = "myproject"
-  
+  type = string
 }
 
-variable "region" {
+variable "environment" {
   type = string
-  default = "us-east-1"
-  
+}
+
+variable "prefix" {
+  type = string
+}
+
+variable "cidr" {
+  type = string
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
 }
--- micro-project/vpc/vpc.tf
+++ micro-project/vpc/vpc.tf
@@ -1,34 +1,24 @@
 module "vpc" {
-  source                 = "terraform-aws-modules/vpc/aws"
-  version                = "5.9.0"
-  name                   = var.vpc_settings.prefix
-  cidr                   = var.vpc_settings.cidr
-  azs                    = var.vpc_settings.azs
-  private_subnets        = var.vpc_settings.private_subnets
-  public_subnets         = var.vpc_settings.public_subnets
-  enable_nat_gateway     = false
-  single_nat_gateway     = false
-  one_nat_gateway_per_az = false
-  enable_dns_hostnames   = false
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.9.0"
+
+  name = var.prefix
+  cidr = var.cidr
+
+  azs             = var.azs
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
+  create_igw = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
-    "Name"    = var.vpc_settings.prefix
-    "project" = var.project_name
-  }
-
-  public_subnet_tags = {
-    "Name" = "public-subnet-${var.vpc_settings.prefix}"
-  }
-
-  private_subnet_tags = {
-    "Name" = "private-subnet-${var.vpc_settings.prefix}"
-  }
-
-  public_route_table_tags = {
-    "Name" = "public-rt-${var.vpc_settings.prefix}"
-  }
-
-  private_route_table_tags = {
-    "Name" = "private-rt-${var.vpc_settings.prefix}"
+    Project     = var.project_name
+    Environment = var.environment
   }
 }
