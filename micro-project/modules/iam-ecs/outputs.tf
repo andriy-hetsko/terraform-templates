6beb8c16e18691e0724166e49f3a3fa3c60dea9b
Andriy Hetsko <andriy.hetsko@boostylabs.com>
2025-12-15 12:39:38 +0100

    fix aim-ecs

--- micro-project/modules/iam-ecs/outputs.tf
+++ micro-project/modules/iam-ecs/outputs.tf
@@ -6,10 +6,3 @@ output "task_role_arn" {
   value = aws_iam_role.task.arn
 }
 
-output "execution_role_arn" {
-  value = aws_iam_role.execution.arn
-}
-
-output "task_role_arn" {
-  value = aws_iam_role.task.arn
-}
