2b515285ab1e67bd6cf14058a0da0b1a23abfd9f
Andriy Hetsko <andriy.hetsko@boostylabs.com>
2025-12-16 09:53:40 +0100

    add ssm

---
+++ micro-project/ec2/outputs.tf
@@ -0,0 +1,11 @@
+output "instance_id" {
+  value       = module.ec2.instance_id
+}
+
+output "private_ip" {
+  value       = module.ec2.private_ip
+}
+
+output "ami_id" {
+  value       = module.ec2.ami_id
+}
