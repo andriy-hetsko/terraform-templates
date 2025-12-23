f266d0e1a41903d5b01a0f57259e93a221f4ce8e
Andriy Hetsko <andriy.hetsko@boostylabs.com>
2025-12-23 17:16:12 +0100

    new ecs module

--- micro-project/alb/outputs.tf
+++ micro-project/alb/outputs.tf
@@ -2,6 +2,6 @@ output "alb_dns_name" {
   value = module.alb.alb_dns_name
 }
 
-output "target_group_arn" {
-  value = module.alb.target_group_arn
+output "target_group_arns" {
+  value = module.alb.target_group_arns
 }
--- micro-project/modules/alb/outputs.tf
+++ micro-project/modules/alb/outputs.tf
@@ -1,4 +1,10 @@
+output "alb_dns_name" {
+  description = "DNS name of the ALB"
+  value       = aws_lb.this.dns_name
+}
+
 output "target_group_arns" {
+  description = "Map of target group ARNs by service name"
   value = {
     for k, tg in aws_lb_target_group.this :
     k => tg.arn
