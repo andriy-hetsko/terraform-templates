23f489a58c77284d97807c480add8a06f1d91797
Andriy Hetsko <andriy.hetsko@boostylabs.com>
2025-12-23 18:33:15 +0100

    new ecs module

--- micro-project/ecs/outputs.tf
+++ micro-project/ecs/outputs.tf
@@ -1,6 +1,6 @@
-output "ecs_cluster_name" {
-  value = module.ecs.ecs_cluster_name
-}
+# output "ecs_cluster_name" {
+#   value = module.ecs.ecs_cluster_name
+# }
 
 # output "ecs_service_name" {
 #   value = module.ecs.ecs_service_name
@@ -10,15 +10,15 @@ output "ecs_cluster_name" {
 #   value = module.ecs.ecs_service_arn
 # }
 
-output "ecs_log_group" {
-  value = module.ecs.log_group_name
-}
-output "services" {
-  value = {
-    for k, m in module.ecs :
-    k => m.service_name
-  }
-}
+# output "ecs_log_group" {
+#   value = module.ecs.log_group_name
+# }
+# output "services" {
+#   value = {
+#     for k, m in module.ecs :
+#     k => m.service_name
+#   }
+# }
 
 output "ecs_services" {
   description = "ECS services created per service"
