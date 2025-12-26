18256823dc1ef9d5f89943d68ae26ca287c82ad9
Andriy Hetsko <andriy.hetsko@boostylabs.com>
2025-12-26 13:30:34 +0100

    update ec2 outputs

--- micro-project/ec2/outputs.tf
+++ micro-project/ec2/outputs.tf
@@ -1,11 +1,20 @@
-output "instance_id" {
-  value       = module.ec2.instance_id
+output "ec2_instance_ids" {
+  value = {
+    for name, mod in module.ec2 :
+    name => mod.instance_id
+  }
 }
 
-output "private_ip" {
-  value       = module.ec2.private_ip
+output "ec2_private_ips" {
+  value = {
+    for name, mod in module.ec2 :
+    name => mod.private_ip
+  }
 }
 
-output "ami_id" {
-  value       = module.ec2.ami_id
+output "ec2_ami_ids" {
+  value = {
+    for name, mod in module.ec2 :
+    name => mod.ami_id
+  }
 }
